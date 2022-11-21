3302b754a1ab4d9db5f6cd1a3d2731ad1bd5c14f
Dmitry Antoshechkin <broker2809@gmail.com>
2022-11-22 00:17:40 +0300

    Task 8 homework added

---
+++ seminar1/task008_Homework/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 2){
+    Console.WriteLine("Нет целых четных чисел больше 1 в диапазоне до " + number);
+}else{
+Console.Write("Четные числа: ");
+for (int i = 2; i <= number; i=i+2){
+    Console.Write(i + " ");
+}
+}
