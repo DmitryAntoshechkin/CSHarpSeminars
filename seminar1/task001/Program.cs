5ba5f37becdfa212e1fbf26d0d00067499529302
Dmitry Antoshechkin <broker2809@gmail.com>
2022-11-21 22:18:21 +0300

    Seminar 1 tasks

---
+++ seminar1/task001/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write ("Введите первое число ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write ("Введите второе число ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if (numberA == numberB * numberB) {
+    Console.WriteLine ("Да");
+
+}else {
+        Console.WriteLine ("Нет");
+}
+
