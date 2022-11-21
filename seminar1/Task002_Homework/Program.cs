132145470b836a52f47c4b9738e1ad064cd56a93
Dmitry Antoshechkin <broker2809@gmail.com>
2022-11-21 23:17:25 +0300

    Task 2 Homework added

---
+++ seminar1/Task002_Homework/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write ("Введите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write ("Введите второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if (numberA == numberB) {
+    Console.WriteLine ("Числа равны");
+}else if (numberA > numberB) {
+        Console.WriteLine ("Первое число больше");
+}
+else {
+        Console.WriteLine ("Второе число больше");
+}
